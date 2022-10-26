3892d786ad23b5073fa5d24d888cd972b301a537
Glitch (curiously-minded-violet-forest-sp-hen) <none>
2022-10-26 16:24:41 +0000

    🗽👜 Checkpoint ./sp-code.js:292339/128:768815/603:13248398/19

--- sp-code.js
+++ sp-code.js
@@ -3,21 +3,29 @@
 export function spCode() {
     let scale = input(1., 0.0, 10.);
     let s = getSpace();
-    let n = noise(s*scale+vec3(0,0,time));
+    let n = (fractalNoise(s*scale+vec3(0,0,time)) + fractalNoise(s*scale+vec3(0,0,time)));
     let r = getRayDirection();
   
-    color(s * n);
+    color(s * vec3(n) * normal + fresnel(3.7));
     rotateX(PI/2 + sin(time));
     sphere(0.2);
+  
+    //intersect();
     box(vec3(.5, .5, .2));
     //difference();
-    //intersect();
+    ///intersect();
     mixGeo(0.4);
     displace(0, 0, 0);
 
+    rotateZ(r.x*15+time*.5);
+    rotateY(r.y*15+time*.5);
+  
     //displace(xPosition, yPosition, zPosition)
-    metal(0.4 * n);
+    shine(noise(s*5)*.8+.2);
+    metal(noise(s*5)*.8+.2);
+  
     torus(.4, .1);
+  
 }
 
 // Hola
