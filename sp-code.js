56c8333fe365a3d26a4bdee75f64fd34aff16406
Glitch (curiously-minded-violet-forest-sp-hen) <none>
2022-10-26 16:07:34 +0000

    Rewound to commit ec2bf08b70479d6bf84a4fff9b93b4e8b2ba2ea3 + 0 edits

--- sp-code.js
+++ sp-code.js
@@ -1,17 +1,7 @@
 // Put your shader park code inside the spCode function
 
 export function spCode() {
-    rotateX(PI/2 + sin(time));
-    sphere(0.2);
-    box(vec3(.5, .5, .2));
-    //difference();
-    //intersect();
-    mixGeo(0.4);
-    displace(0, 0, 0);
-
-      //displace(xPosition, yPosition, zPositio
-    metal(0.4);
-    torus(.4, .1);
+    sphere(.5);
 }
 
 // Hola
