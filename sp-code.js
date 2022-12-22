95e7cd397c40db5655f7d704fec0a943690d5393
Glitch (curiously-minded-violet-forest-sp-hen) <none>
2022-12-21 21:32:02 -0500

    noise:

--- sp-code.js
+++ sp-code.js
@@ -7,27 +7,28 @@ export function spCode() {
     return Math.floor(fxrand()* (max - min + 1) + min)
   }
   
-  let scale = randomIntFromInterval(2.0, 5.0);
+  let scale = randomIntFromInterval(2.0, 4.0);
   let a = randomIntFromInterval(0, 3.0);
   let b = randomIntFromInterval(0, 5.0);
   let e = randomIntFromInterval(0, 5.0);
   let f = randomIntFromInterval(0.0, 5.0);
-  let g = randomIntFromInterval(1.0, 1.5);
+  let g = randomIntFromInterval(1.0, 1.2);
   let h = randomIntFromInterval(1.2, 2.5);
   
-  let red = randomIntFromInterval(0.5, 2.5);
+  let red = randomIntFromInterval(0.5, 2.0);
   let green = randomIntFromInterval(0.5, 2.3);
   let blue = randomIntFromInterval(0.5, 5.5);
   
   let wings = shape(() => {
     let s = getSpace();
     let r = getRayDirection();
-    let n = noise(s*scale+vec3(0, 9.0,time*b));
+    let n = noise(s*scale+vec3(2.0,5.9,time*0.1) + noise(s*scale+vec3(0, 9.0,time*b)));
+    let nn = noise(s*scale+vec3(0, 9.0,b));
     setStepSize(.2);
   
     rotateX(PI/2 + (sin(a + time)) + a);
   
-    color(vec3(n)+vec3(red, green, blue)* fresnel(h));
+    color(vec3(nn)+vec3(red, green, blue)* fresnel(h));
     box(vec3(f+time, f+time, .2));
   
     mirrorN(g, 0.3);
